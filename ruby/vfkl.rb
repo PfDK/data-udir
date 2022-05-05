0a16574107627fb08fa69c73009d544172d345f6
Erlend Thune <erlend.thune@udir.no>
2022-05-05 09:04:33 +0200

    VFKL

--- ruby/vfkl.rb
+++ ruby/vfkl.rb
@@ -1,9 +1,5 @@
 #Author    : Erlend Thune (erlend.thune@udir.no)
-#Purpose   : Finn alle ord i læreplanene i mappen som scriptet kjøres fra. 
-#            Lag input filer for ordskyer og lag html filer som viser ordskyene. 
-#            Lag index.html fil som lenker til alle ordskyene.
-#Dependency: Avhenger av at alle læreplanene er lastet ned som fil 
-#The code below is based on https://gist.githubusercontent.com/kyletcarlson/7911188/raw/34a01eaf7e4c6cdaeb0ac0f479cad9deae7311ff/json_response_handling_ruby.rb
+#Purpose   : Lag strukturert oversikt over alle læreplaner i grunn- og videregående skole til bruk i VFKL.
 
 # http://ruby-doc.org/stdlib-2.0.0/libdoc/open-uri/rdoc/OpenURI.html 
 require 'open-uri'
@@ -14,29 +10,18 @@ require 'pp'
 require_relative 'udirutility'  
 
 
-def OpenFile(filename)
-	$file = File.open( filename,"w" )
-end
-
-def CloseFile()
-	$file.close
-end
-
-def myputs(s)
-	$file << s
-end
-
-
 def getJsonFromUrl(url, kode)
     buffer = nil
     result = nil
+
+    fileName = kode + ".json"
     begin
-        buffer = open(kode).read
+        buffer = open(fileName).read
     rescue
 #        puts "Local file #{kode} not found"
 #        puts "Open " + url
         buffer = open(url).read
-        open(kode, 'wb') do |file|
+        open(fileName, 'wb') do |file|
             file << buffer
             file.close
         end  
@@ -49,28 +34,170 @@ def getJsonFromUrl(url, kode)
 	return result
 end
 
+def createCodeObject(value, label)
+    return { "Value" => "#{value}", "Label" =>  label}
+end
+
+def HandleProgramOmraader(alle, lpInnhold, opplaeringsnivaa, fagtype, opplaeringsfagLaereplanKode, opplaeringsfagLaereplanTittel, opplaeringsfagLaereplanUrl)
+    lpInnhold['programomraader-referanse'].each do |programomraadeReferanse|
+        programomraadeReferanseKode = programomraadeReferanse['kode']
+        programomraadeReferanseUrl = programomraadeReferanse['url-data']
+        programomraade = getJsonFromUrl(programomraadeReferanseUrl, programomraadeReferanseKode)
+    
+        programomraade["utdanningsprogram-referanse"].each do |utdanningsProgram|
+            utdanningsProgramTittel = utdanningsProgram["tittel"]
+
+            utdanningsProgramKode = utdanningsProgram["kode"]
+            utdanningsProgramUrl = utdanningsProgram["url-data"]
+
+            utdanningsProgram = getJsonFromUrl(utdanningsProgramUrl, utdanningsProgramKode)
+
+            typeUtdanningsProgram = utdanningsProgram["type-utdanningsprogram"]["beskrivelse"][0]["verdi"]
+
+            if(!alle[opplaeringsnivaa][typeUtdanningsProgram])
+                alle[opplaeringsnivaa][typeUtdanningsProgram] = {}
+            end
+            
+            if(!alle[opplaeringsnivaa][typeUtdanningsProgram][utdanningsProgramTittel])
+                alle[opplaeringsnivaa][typeUtdanningsProgram][utdanningsProgramTittel] = {}
+            end
+
+            if(!alle[opplaeringsnivaa][typeUtdanningsProgram][utdanningsProgramTittel][fagtype])
+                alle[opplaeringsnivaa][typeUtdanningsProgram][utdanningsProgramTittel][fagtype] = {}
+            end
+
+            alle[opplaeringsnivaa][typeUtdanningsProgram][utdanningsProgramTittel][fagtype][opplaeringsfagLaereplanTittel] = opplaeringsfagLaereplanKode
+ #           alleSorted = alle[opplaeringsnivaa][typeUtdanningsProgram][utdanningsProgramTittel][fagtype].sort_by {|k, v| v}.to_h
+#            alle[opplaeringsnivaa][typeUtdanningsProgram][utdanningsProgramTittel][fagtype] = alleSorted
+
+
+        end
+    end 
+end
+
+def HandleLp(alle, lpInnhold, opplaeringsnivaa, fagtype, opplaeringsfagLaereplanKode, opplaeringsfagLaereplanTittel, opplaeringsfagLaereplanUrl)
+#                if(gyldigfra)
+#                    opplaeringsfagLaereplanTittel << " Gyldig fra " + gyldigfra
+#                end
+#                if(gyldigtil)
+#                    opplaeringsfagLaereplanTittel << " Gyldig til" + gyldigtil
+#                end
+#                lp = getJsonFromUrl(opplaeringsfagLaereplanUrl, opplaeringsfagLaereplanKode)
+
+
+#                if(!lp["kortform"])
+#                    puts lp["url-data"]
+#                    next
+#                end
+    #puts lp["kortform"][0]["verdi"]
+    #puts "HALLO"
+
+    #                opplaeringsfagLaereplanTittel = lp["kortform"][0]["verdi"] + " (" + opplaeringsfagLaereplanKode + ")"
+    lpProcessed = {}
+
+    if(lpInnhold['programomraader-referanse'].length > 0) 
+        HandleProgramOmraader(alle, lpInnhold, opplaeringsnivaa, fagtype, opplaeringsfagLaereplanKode, opplaeringsfagLaereplanTittel, opplaeringsfagLaereplanUrl)
+    else
+        if(opplaeringsnivaa.include? "videregaaende")
+            puts "Videregående fag mangler programområde #{opplaeringsfagLaereplanKode} nivå:#{opplaeringsnivaa}"
+        end
+        if(!alle[opplaeringsnivaa][fagtype])
+            alle[opplaeringsnivaa][fagtype] = {}
+        end
+        alle[opplaeringsnivaa][fagtype][opplaeringsfagLaereplanTittel] = opplaeringsfagLaereplanKode
+#        alleSorted = alle[opplaeringsnivaa][fagtype].sort_by {|k, v| v}.to_h
+ #       alle[opplaeringsnivaa][fagtype] = alleSorted
+
+#        if(!lpProcessed[opplaeringsfagLaereplanUrl])
+#            lpProcessed[opplaeringsfagLaereplanUrl] = true
+
+#            alle[opplaeringsnivaa][opplaeringsfagLaereplanTittel] = opplaeringsfagLaereplanKode
+#        end
+    end
+    print "."
+end
+
+def outputFagomraade(navn, fagomraade)
+    o = []
+    fagomraade.each do |k, v|   
+        o.push(createCodeObject("#{k}, #{v}", "#{k} (#{v})"))
+    end       
+    output(navn, o)
+end
+
+def outputUtdanningsprogram(navn, utdanningsprogram)
+    o = []
+    utdanningsprogram.each do |k, v|   
+        o.push(createCodeObject(k,k))
+        outputFagomraade("#{navn}#{k}", v)
+    end       
+    output(navn, o)
+end
+
+def outputProgramomraade(navn, programomraade)
+    o = []
+    programomraade.each do |k, v|   
+        o.push(createCodeObject(k,k))
+        outputUtdanningsprogram("#{navn}#{k}", v)
+    end       
+    output(navn, o)
+end
+
+def outputProgramomraader(navn, programomraader)
+    o = []
+    programomraader.each do |k, v|   
+        o.push(createCodeObject(k,k))
+        outputProgramomraade("#{navn}#{k}", v)
+    end       
+    output(navn, o)
+end
+
+def outputGrunnskole(navn, grunnskole)
+    o = []
+    grunnskole.each do |k, v|   
+        o.push(createCodeObject(k,k))
+        outputFagomraade("#{navn}#{k}", v)
+    end
+    output(navn, o)
+end
+
+
+def output(name, o)
+    open("#{name}.json", 'wb') do |file|
+        oSorted = o.sort_by{ |e| e["Label"] }
+        file << oSorted.to_json
+        file.close
+    end  
+end
+
 alle = {}
 manglerData = {}
 
+puts "Get opplæringsfag."
 result = getJsonFromUrl('https://data.udir.no/kl06/v201906/opplaeringsfag.json', 'opplaeringsfag.json')
 
 result.each do |fag|
-  if(!fag['status'].include? "utgaatt")
-    lpInnhold = getJsonFromUrl(fag["url-data"], fag["kode"])
+    if(!fag['status'].include? "utgaatt")
+        lpInnhold = getJsonFromUrl(fag["url-data"], fag["kode"])
 
-    #puts "Opplæringsfag: #{fag["url-data"]}"
 
-    opplaeringsfag = lpInnhold["tittel"][0]["verdi"]
-    opplaeringsnivaa = lpInnhold["opplaeringsnivaa"]["tittel"]
+        opplaeringsfag = lpInnhold["tittel"][0]["verdi"]
+        opplaeringsnivaa = lpInnhold["opplaeringsnivaa"]["kode"]
+        fagtype = lpInnhold["fagtype"]["tittel"]
 
-    if(!alle[opplaeringsnivaa]) 
-        alle[opplaeringsnivaa] = {}
-    end
+        if(!alle[opplaeringsnivaa]) 
+            alle[opplaeringsnivaa] = {}
+        end
 
-    lpInnhold["laereplan-referanse"].each do |laereplanreferanse|        
-        begin
+        lpInnhold["laereplan-referanse"].each do |laereplanreferanse|        
             opplaeringsfagLaereplanKode = laereplanreferanse["kode"]
             opplaeringsfagLaereplanTittel = laereplanreferanse["tittel"]
+            opplaeringsfagLaereplanTittel.sub!("Læreplan i ", "")
+            opplaeringsfagLaereplanTittel.sub!("Læreplan for ", "")
+            letters = opplaeringsfagLaereplanTittel.split('')
+            letters.first.upcase!
+            opplaeringsfagLaereplanTittel = letters.join # + " (" + opplaeringsfagLaereplanKode + ")"
+            
             opplaeringsfagLaereplanUrl = laereplanreferanse["url-data"]
 
             gyldigLaereplan = false
@@ -80,57 +207,25 @@ result.each do |fag|
             elsif (gyldigtil > "2022-04-10T00:00:00") 
                 gyldigLaereplan = true
             end
-    
+            gyldigfra = laereplanreferanse["gyldighet"]["gyldig-fra"]
+
             if(gyldigLaereplan)
-                if(lpInnhold['programomraader-referanse'].length > 0) 
-                    lpInnhold['programomraader-referanse'].each do |programomraadeReferanse|
-                        programomraadeReferanseKode = programomraadeReferanse['kode']
-                        programomraadeReferanseUrl = programomraadeReferanse['url-data']
-                        programomraade = getJsonFromUrl(programomraadeReferanseUrl, programomraadeReferanseKode)
-                    
-                        programomraade["utdanningsprogram-referanse"].each do |utdanningsProgram|
-                            utdanningsProgramTittel = utdanningsProgram["tittel"]
-
-                            utdanningsProgramKode = utdanningsProgram["kode"]
-                            utdanningsProgramUrl = utdanningsProgram["url-data"]
-
-                            utdanningsProgram = getJsonFromUrl(utdanningsProgramUrl, utdanningsProgramKode)
-
-                            typeUtdanningsProgram = utdanningsProgram["type-utdanningsprogram"]["beskrivelse"][0]["verdi"]
-
-                            if(!alle[opplaeringsnivaa][typeUtdanningsProgram])
-                                alle[opplaeringsnivaa][typeUtdanningsProgram] = {}
-                            end
-                            
-                            if(!alle[opplaeringsnivaa][typeUtdanningsProgram][utdanningsProgramTittel])
-                                alle[opplaeringsnivaa][typeUtdanningsProgram][utdanningsProgramTittel] = {}
-                            end
-
-                            alle[opplaeringsnivaa][typeUtdanningsProgram][utdanningsProgramTittel][opplaeringsfagLaereplanKode] = opplaeringsfagLaereplanTittel
-                        end
-                    end 
-                else
-                    alle[opplaeringsnivaa][opplaeringsfagLaereplanKode] = opplaeringsfagLaereplanTittel
-                end
+                HandleLp(alle, lpInnhold, opplaeringsnivaa, fagtype, opplaeringsfagLaereplanKode, opplaeringsfagLaereplanTittel, opplaeringsfagLaereplanUrl)
             end
-        rescue
-            puts fag["url-data"]
-            manglerData[fag["kode"]] = fag["url-data"]
         end
     end
-  end
 end
-#alleUtdanningsProgram.each do |utdanningsProgram, laereplan|
-#    appFileBuffer = laereplan.to_json
-    open("vfkl.json", 'wb') do |file|
-        file << alle.to_json
-        file.close
-    end  
+
+
+outputGrunnskole("Grunnskole", alle["opplaeringsnivaa_grunnskole"])
+outputProgramomraader("Videregaaende", alle["opplaeringsnivaa_videregaaende"])
+#    outputProgramomraade(programomraade)
+#    programomraade.each do |program|
+#        outputProgram(program)
+#    end
 #end
 
-
-
-  
-
- 
-
+open("grepManglerData.json", 'wb') do |file|
+    file << manglerData.to_json
+    file.close
+end  
